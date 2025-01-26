c27f6bef3b54f508e2faf6821bf3525a83241139
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-26 21:11:21 +0000

    Basic loop built

--- chat.rb
+++ chat.rb
@@ -6,26 +6,31 @@ require "dotenv/load"
 
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
-#pp client
+pp "How can I help you today?  To end the conversation, type solely 'bye'."
 
-message_list=
-[
-    {
-      "role" => "system",
-      "content" => "You are a helpful assistant who talks like Oscar Wilde."
-    }, 
-    {
-      "role" => "user",
-      "content" => "Hello, what are the best spots for pizza in Chicago?"
+#get response from user to send to GPT - while user does not input "bye"
+
+user_input = gets.chomp
+while user_input != "bye"
+  message_list=
+  [
+      {
+        "role" => "system",
+        "content" => "You are a helpful assistant who talks like Oscar Wilde."
+      }, 
+      {
+        "role" => "user",
+        "content" => "#{user_input}"
+      }
+      
+  ]
+
+  #Call the API to get the next message from GPT
+  api_response = client.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: message_list
     }
-    
-]
-
-#Call the API to get the next message from GPT
-api_response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: message_list
-  }
-)
-pp api_response
+  )
+  pp api_response
+end
