a981c2715714ff1f14d9e5411dd939bb66ba8aeb
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-26 21:05:06 +0000

    setup complete

--- chat.rb
+++ chat.rb
@@ -1,1 +1,31 @@
 # Write your solution here!
+require "http"
+require "json"
+require "openai"
+require "dotenv/load"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+#pp client
+
+message_list=
+[
+    {
+      "role" => "system",
+      "content" => "You are a helpful assistant who talks like Oscar Wilde."
+    }, 
+    {
+      "role" => "user",
+      "content" => "Hello, what are the best spots for pizza in Chicago?"
+    }
+    
+]
+
+#Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+pp api_response
