658cb39d93e406dded9812f805879c78abf1480e
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-01-27 03:27:27 +0000

    Part 3 Complete

--- chat.rb
+++ chat.rb
@@ -4,6 +4,13 @@ require "json"
 require "openai"
 require "dotenv/load"
 
+history = 
+[
+  { "role" => "system",
+    "content" => "You are a helpful assistant who talks like Oscar Wilde."
+  }
+]
+
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
 puts "How can I help you today?  To end the conversation, type solely 'bye'."
@@ -13,28 +20,24 @@ puts "-" * 50
 
 user_input = gets.chomp
 while user_input != "bye"
-  message_list=
-  [
-      {
-        "role" => "system",
-        "content" => "You are a helpful assistant who talks like Oscar Wilde."
-      }, 
-      {
-        "role" => "user",
-        "content" => "#{user_input}"
-      }
-      
-  ]
+  history.push({"role" => "user", "content" => user_input})
 
   #Call the API to get the next message from GPT
   api_response = client.chat(
     parameters: {
       model: "gpt-3.5-turbo",
-      messages: message_list
+      messages: history
     }
   )
 
-  puts api_response.fetch("choices").at(0).fetch("message").fetch("content")
+  chatbot_response = api_response.fetch("choices").at(0).fetch("message").fetch("content")
+  puts chatbot_response
   puts "-" * 50
   user_input = gets.chomp
+
+  #Storing responses if conversation is continuing
+  if user_input != "bye"
+    history.push({"role" => "system", "content" => chatbot_response})
+  end
 end
+puts "Goodbye!"
